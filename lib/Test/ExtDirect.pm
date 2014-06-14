0681f0bc2bbbeb5a1e5053e0b70e803c5fd5d3d3
Alex Tokarev <tokarev@cpan.org>
2014-06-14 02:15:07 +0000

    Import of TOKAREV/Test-ExtDirect-1.00 from CPAN.
    gitpan-cpan-distribution: Test-ExtDirect
    gitpan-cpan-version:      1.00
    gitpan-cpan-path:         TOKAREV/Test-ExtDirect-1.00.tar.gz
    gitpan-cpan-author:       TOKAREV
    gitpan-cpan-maturity:     released

--- lib/Test/ExtDirect.pm
+++ lib/Test/ExtDirect.pm
@@ -1,21 +1,22 @@
 package Test::ExtDirect;
 
-use 5.006000;
 use strict;
 use warnings;
 
+use Carp;
+use Data::Dumper; # for cloning
 use Exporter;
 
 use Test::More;
-use Carp;
-use Clone ();
 
 use RPC::ExtDirect::Server;
+use RPC::ExtDirect::Server::Util;
 use RPC::ExtDirect::Client;
 
 our @ISA = qw(Exporter);
 
 our @EXPORT_OK = qw(
+    maybe_start_server
     start_server
     stop_server
 
@@ -42,18 +43,19 @@ our %EXPORT_TAGS = (
     DEFAULT => [qw/
         start_server stop_server call_extdirect call_extdirect_ok
         submit_extdirect submit_extdirect_ok poll_extdirect
-        poll_extdirect_ok get_extdirect_api
+        poll_extdirect_ok get_extdirect_api maybe_start_server
     /],
 
     all => [qw/
         start_server stop_server call_extdirect call_extdirect_ok
         submit_extdirect submit_extdirect_ok poll_extdirect
         poll_extdirect_ok call submit poll call_ok submit_ok poll_ok
-        get_extdirect_api
+        get_extdirect_api maybe_start_server
     /],
 );
 
 our @EXPORT = qw(
+    maybe_start_server
     start_server
     stop_server
 
@@ -66,67 +68,29 @@ our @EXPORT = qw(
     poll_extdirect_ok
 );
 
-our $VERSION = '0.24';
-$VERSION = eval $VERSION;
-
-our ($SERVER_PID, $SERVER_PORT);
+our $VERSION = '1.00';
 
 ### PUBLIC PACKAGE SUBROUTINE (EXPORT) ###
 #
-# Starts testing HTTP server and returns the port it listens on
+# Starts testing HTTP server and returns the host and port
+# the server is listening on.
 #
 
-sub start_server {
-    my (%params) = @_;
-
-    return $SERVER_PORT if $SERVER_PORT;
-
-    my $class = delete $params{server_class} || 'RPC::ExtDirect::Server';
-
-    eval "require $class" or croak "Can't load package $class";
-
-    my $server = $class->new(%params);
-    my $port   = $SERVER_PORT = $server->port;
-
-    if ( my $pid = $SERVER_PID = fork ) {
-        local $SIG{CHLD} = sub { waitpid $pid, 0 };
-
-        # Give child some time to start
-        select undef, undef, undef, 0.1;
-
-        return $port;
-    }
-    elsif ( defined $pid && $pid == 0 ) {
-
-        # Trap last breaths to avoid cluttering the screen
-        local $SIG{__DIE__} = sub {};
-
-        $server->run();
-
-        exit 0;
-    }
-    else {
-        die "Can't fork: $!";
-    };
-
-    return;     # Just in case
-}
+*start_server = *RPC::ExtDirect::Server::Util::start_server;
 
 ### PUBLIC PACKAGE SUBROUTINE (EXPORT) ###
 #
 # Stops the running HTTP server instance
 #
 
-sub stop_server {
-    my $pid = shift || $SERVER_PID;
+*stop_server = *RPC::ExtDirect::Server::Util::stop_server;
 
-    # This is a bit kludgy but somehow if I use any other signal
-    # the server kid has last opportunity to cry for help.
-    # Which we don't want.
-    kill 9, $pid if defined $pid;
+### PUBLIC PACKAGE SUBROUTINE (EXPORT) ###
+#
+# Potentially starts an instance of a Server.
+#
 
-    $SERVER_PID = $SERVER_PORT = undef;
-}
+*maybe_start_server = *RPC::ExtDirect::Server::Util::maybe_start_server;
 
 ### PUBLIC PACKAGE SUBROUTINE (EXPORT) ###
 #
@@ -135,8 +99,13 @@ sub stop_server {
 #
 
 sub get_extdirect_api {
-    my $client = _get_client();
-    my $api    = $client->api();
+    my (%params) = @_;
+    
+    # We assume that users want remoting API by default
+    my $api_type = delete $params{type} || 'remoting';
+    
+    my $client = _get_client(%params);
+    my $api    = $client->get_api($api_type);
 
     return $api;
 }
@@ -150,13 +119,53 @@ sub get_extdirect_api {
 sub call_extdirect {
     my (%params) = @_;
 
-    my $action = delete $params{action};
-    my $method = delete $params{method};
-    my $arg    = Clone::clone( delete $params{arg} );
+    my $action_name = delete $params{action};
+    my $method_name = delete $params{method};
+    my $arg         = _clone( delete $params{arg} );
 
     my $client = _get_client(%params);
-    my $data   = $client->call(action => $action, method => $method,
-                               arg    => $arg,    %params);
+    
+    # This is a backward compatibility measure; until RPC::ExtDirect 3.0 the
+    # calling code wasn't required to pass any arg to the client when calling
+    # a method with ordered parameters. It is now an error to do so, and
+    # for a good reason: starting with version 3.0, it is possible to
+    # define a method with no strict argument checking, which defaults to
+    # using named parameters. To avoid possible problems stemming from this
+    # change, we strictly check the existence of arguments for both ordered
+    # and named conventions in RPC::ExtDirect::Client.
+    #
+    # Having said that, I don't think that this kind of strict checking is
+    # beneficial for Test::ExtDirect since the test code that calls
+    # Ext.Direct methods is probably focusing on other aspects than strict
+    # argument checking that happens in the transport layer.
+    #
+    # As a side benefit, we also get an early warning if something went awry
+    # and we can't even get a reference to the Action or Method in question.
+    
+    if ( !$arg ) {
+        my $api = $client->get_api('remoting');
+        
+        croak "Can't get remoting API from the client" unless $api;
+    
+        my $method = $api->get_method_by_name($action_name, $method_name);
+        
+        croak "Can't resolve ${action_name}->${method_name} method"
+            unless $method;
+        
+        if ( $method->is_ordered ) {
+            $arg = [];
+        }
+        elsif ( $method->is_named ) {
+            $arg = {};
+        }
+    }
+    
+    my $data = $client->call(
+        action => $action_name,
+        method => $method_name,
+        arg    => $arg,
+        %params,
+    );
 
     return $data;
 }
@@ -169,9 +178,11 @@ sub call_extdirect {
 #
 
 sub call_extdirect_ok {
+    local $@;
+    
     my $result = eval { call_extdirect(@_) };
 
-    _pass_or_fail($@);
+    _pass_or_fail(my $err = $@);
 
     return $result;
 }
@@ -188,8 +199,8 @@ sub submit_extdirect {
 
     my $action = delete $params{action};
     my $method = delete $params{method};
-    my $arg    = Clone::clone( delete $params{arg}    );
-    my $upload = Clone::clone( delete $params{upload} );
+    my $arg    = _clone( delete $params{arg}    );
+    my $upload = _clone( delete $params{upload} );
 
     my $client = _get_client(%params);
     my $data   = $client->submit(action => $action, method => $method,
@@ -207,9 +218,11 @@ sub submit_extdirect {
 #
 
 sub submit_extdirect_ok {
+    local $@;
+    
     my $result = eval { submit_extdirect(@_) };
 
-    _pass_or_fail($@);
+    _pass_or_fail(my $err = $@);
 
     return $result;
 }
@@ -238,9 +251,11 @@ sub poll_extdirect {
 #
 
 sub poll_extdirect_ok {
+    local $@;
+    
     my $result = eval { poll_extdirect(@_) };
 
-    _pass_or_fail($@);
+    _pass_or_fail(my $err = $@);
 
     return $result;
 }
@@ -263,10 +278,9 @@ sub _get_client {
 
     $params{static_dir} ||= '/tmp';
 
-    my $host = $params{host} || '127.0.0.1';
-    my $port = $params{port} || start_server(%params);
+    my ($host, $port) = maybe_start_server(%params);
 
-    my $client = $class->new(host => $host, port => $port);
+    my $client = $class->new(host => $host, port => $port, %params);
 
     return $client;
 }
@@ -289,243 +303,23 @@ sub _pass_or_fail {
     };
 }
 
-END { stop_server }
+### PRIVATE PACKAGE SUBROUTINE ###
+#
+# Create a deep copy (clone) of the passed data structure.
+# We're not much concerned with performance here, and this
+# custom implementation allows to avoid a depedency like
+# Clone or Storable, which is overkill here.
+#
+
+sub _clone {
+    my $data = shift;
+    
+    # Faster than calling instance methods
+    local $Data::Dumper::Purity   = 1;
+    local $Data::Dumper::Terse    = 1;
+    local $Data::Dumper::Deepcopy = 1;
+    
+    return eval Dumper($data);
+}
 
 1;
-
-__END__
-
-=pod
-
-=head1 NAME
-
-Test::ExtDirect - An easy and convenient way to test Ext.Direct classes
-
-=head1 SYNOPSIS
-
-With default imports:
-
-    use Test::ExtDirect;
-    
-    my $data = call_extdirect(client_class => 'My::ExtDirect::Client',
-                              action       => 'Action',
-                              method       => 'Method',
-                              arg          => { foo => 'bar' },
-                              cookies      => { bar => 'baz' });
-    
-    $data = submit_extdirect(action => 'Action',
-                             method => 'form_handler',
-                             arg    => { field1 => 'value1' });
-    
-    $data = poll_extdirect();
-
-Or:
-
-    use Test::ExtDirect qw(:all);
-    
-    my $data = call(...);
-    $data    = submit(...);
-    $data    = poll();
-
-=head1 DESCRIPTION
-
-This module provides a set of utility functions for testing Ext.Direct
-classes. For each test script, an instance of L<RPC::ExtDirect::Server>
-will be created, and requests will be made with L<RPC::ExtDirect::Client>.
-
-This way you can simulate actual Ext JS or Sencha Touch client code making
-calls, submitting forms, uploading files, or polling events. Such testing
-provides additional layer of insurance against Ext.Direct specific problems
-and errors that may otherwise creep into the codebase.
-
-=head1 SUBROUTINES
-
-Test::ExtDirect provides the following subroutines:
-
-=over 4
-
-=item start_server(%params)
-
-Starts a new RPC::ExtDirect::Server instance. It is not necessary to call
-this function directly, call/submit/poll will launch a new server if it's
-not done yet.
-
-=over 8
-
-=item server_class
-
-Specifies server class to be used instead of default RPC::ExtDirect::Server.
-
-=item other
-
-Other parameters are accepted as hash and passed on to server constructor.
-See L<RPC::ExtDirect::Server> for details.
-
-=back
-
-Returns port number that server listens on.
-
-=item stop_server
-
-Stops the server instance used for testing. Again it's not necessary to call
-this function explicitly, the server will be shut down when all tests are
-finished.
-
-=item get_extdirect_api
-
-Returns Ext.Direct API published by the server as
-L<RPC::ExtDirect::Client::API> object.
-
-=item call_extdirect(%params)
-
-Call Ext.Direct remoting method. Parameters:
-
-=over 8
-
-=item client_class
-
-Client class to use instead of default RPC::ExtDirect::Client.
-
-=item action
-
-Ext.Direct Action (class) name.
-
-=item method
-
-Ext.Direct Method name.
-
-=item arg
-
-Method arguments; either arrayref for Methods that accept ordered arguments
-or hashref for Methods that request named arguments.
-
-=item ...
-
-Any other parameters are passed to client constructor. See
-L<RPC::ExtDirect::Client> for details.
-
-=back
-
-Returns whatever data is returned by Method.
-
-=item submit_extdirect
-
-Submit an Ext.Direct form request. Takes the following parameters:
-
-=over 8
-
-=item client_class, action, method, arg
-
-The same as with L</call_extdirect>, see above.
-
-=item upload
-
-Arrayref of file paths to upload with that request.
-
-=item ...
-
-Any other parameters are passed to client constructor. See
-L<RPC::ExtDirect::Client> for details.
-
-=back
-
-Returns the data passed on by Method.
-
-=item poll_extdirect
-
-Poll the server for events. Takes the following parameters:
-
-=over 8
-
-=item client_class
-
-The same as with L</call_extdirect>, see above.
-
-=item ...
-
-Any other parameters are passed to client constructor. See
-L<RPC::ExtDirect::Client> for details.
-
-=back
-
-Returns arrayref with event data. When there are no events ready (if you
-test this case), a single __NONE__ event will be returned. This is to avoid
-breaking Ext JS code that throws an exception if no events are returned by
-the server.
-
-=back
-
-=head1 EXPORT
-
-By default, the following functions are exported:
-
-=over 4
-
-=item start_server
-
-=item stop_server
-
-=item call_extdirect
-
-=item submit_extdirect
-
-=item poll_extdirect
-
-=back
-
-There are short-named versions that are not exported by default to avoid
-collisions. They can be added with :all tag:
-
-=over 4
-
-=item call
-
-Points to call_extdirect.
-
-=item submit
-
-Points to submit_extdirect.
-
-=item poll
-
-Points to poll_extdirect.
-
-=back
-
-For each of call*, submit* and poll*, there is similarly named *_ok
-function that wraps its respective namesake in eval(), passing or
-failing additional test depending on eval() success.
-
-=head1 DEPENDENCIES
-
-Test::ExtDirect is dependent on the following modules: L<Clone>,
-L<Test::More>, L<RPC::ExtDirect::Server>, L<RPC::ExtDirect::Client>.
-
-=head1 BUGS AND LIMITATIONS
-
-There are no known bugs in this module. Please report problems to author,
-patches are welcome.
-
-=head1 SEE ALSO
-
-For more information on developing Ext.Direct code in Perl, see
-L<RPC::ExtDirect>.
-
-=head1 AUTHOR
-
-Alexander Tokarev E<lt>tokarev@cpan.orgE<gt>
-
-=head1 ACKNOWLEDGEMENTS
-
-I would like to thank IntelliSurvey, Inc for sponsoring my work
-on this module.
-
-=head1 COPYRIGHT AND LICENSE
-
-Copyright (c) 2012 by Alexander Tokarev
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself. See L<"perlartistic">.
-
-=cut
-
